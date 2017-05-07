e5f228d584e9a34d85b7dfe36aca01cf8d4d19ba
Amr Ayman Ayoub <amrayman1907@gmail.com>
2017-05-07 16:16:17 +0200

    the Widget worked , but still need more enhancing

--- app/src/main/java/com/udacity/stockhawk/StockHawkWidgetFactory.java
+++ app/src/main/java/com/udacity/stockhawk/StockHawkWidgetFactory.java
@@ -4,10 +4,17 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Binder;
+import android.os.Build;
+import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import com.udacity.stockhawk.data.Contract;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 
 /**
@@ -17,8 +24,25 @@ import com.udacity.stockhawk.data.Contract;
 public class StockHawkWidgetFactory implements RemoteViewsService.RemoteViewsFactory {
     private Cursor mCursor;
     private Context mContext;
+
     //List<String>Symbols,Prices,Changes;
     int mWidgetId;
+    public final String LOG_TAG = StockHawkWidgetService.class.getSimpleName();
+    private static final String[] Quotes_COLUMNS = {
+            Contract.Quote.TABLE_NAME + "." + Contract.Quote._ID,
+            Contract.Quote.COLUMN_SYMBOL,
+            Contract.Quote.COLUMN_PRICE,
+            Contract.Quote.COLUMN_ABSOLUTE_CHANGE,
+            Contract.Quote.COLUMN_PERCENTAGE_CHANGE,
+            Contract.Quote.COLUMN_HISTORY
+    };
+    // these indices must match the projection
+    static final int INDEX_QUOTE_ID = 0;
+    static final int INDEX_QUOTE_SYMBOL = 1;
+    static final int INDEX_QUOTE_PRICE = 2;
+    static final int INDEX_QUOTE_ABS_CHANGE = 3;
+    static final int INDEX_QUOTE_PREC_CHANGE = 4;
+    static final int INDEX_QUOTE_HISTORY = 5;
 
     public StockHawkWidgetFactory(Context context, Intent intent) {
         mContext = context;
@@ -27,17 +51,12 @@ public class StockHawkWidgetFactory implements RemoteViewsService.RemoteViewsFac
 
     @Override
     public void onCreate() {
-        mCursor = mContext.getContentResolver().query(
-                Contract.Quote.URI,
-                new String[]{Contract.Quote.COLUMN_SYMBOL,Contract.Quote.COLUMN_PRICE,Contract.Quote.COLUMN_ABSOLUTE_CHANGE}
-                ,null
-                ,null
-                ,null);
     }
 
     @Override
     public int getCount() {
-        return mCursor.getCount();
+        return mCursor == null ? 0 : mCursor.getCount();
+
     }
 
     @Override
@@ -52,12 +71,19 @@ public class StockHawkWidgetFactory implements RemoteViewsService.RemoteViewsFac
 
     @Override
     public RemoteViews getViewAt(int position) {
-        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_list_item);
-        if (mCursor.moveToPosition(position)) {
-            rv.setTextViewText(R.id.stock_symbol, mCursor.getString(Contract.Quote.POSITION_SYMBOL));
-            rv.setTextViewText(R.id.price, mCursor.getString(Contract.Quote.POSITION_PRICE));
-            rv.setTextViewText(R.id.stock_change, mCursor.getString(Contract.Quote.POSITION_ABSOLUTE_CHANGE));
+        if (position == AdapterView.INVALID_POSITION ||
+                mCursor == null || !mCursor.moveToPosition(position)) {
+            return null;
         }
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_list_item);
+        String symbol = mCursor.getString(INDEX_QUOTE_SYMBOL);
+        String price = Float.toString(mCursor.getFloat(INDEX_QUOTE_PRICE)),
+                change = Float.toString(mCursor.getFloat(INDEX_QUOTE_ABS_CHANGE));
+
+            rv.setTextViewText(R.id.stock_symbol, symbol);
+            rv.setTextViewText(R.id.price, price );
+            rv.setTextViewText(R.id.stock_change, change);
+
         return rv;
     }
 
@@ -65,7 +91,6 @@ public class StockHawkWidgetFactory implements RemoteViewsService.RemoteViewsFac
     public int getViewTypeCount() {
         return 1;
     }
-
     @Override
     public boolean hasStableIds() {
         return true;
@@ -73,15 +98,24 @@ public class StockHawkWidgetFactory implements RemoteViewsService.RemoteViewsFac
 
     @Override
     public void onDataSetChanged() {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+        final long identityToken = Binder.clearCallingIdentity();
         mCursor = mContext.getContentResolver().query(
                 Contract.Quote.URI,
-                new String[]{Contract.Quote.COLUMN_SYMBOL,Contract.Quote.COLUMN_PRICE,Contract.Quote.COLUMN_ABSOLUTE_CHANGE}
-                ,null
-                ,null
-                ,null);
+                Quotes_COLUMNS,
+                null,
+                null,
+                Contract.Quote.COLUMN_SYMBOL + " ASC");
+        Binder.restoreCallingIdentity(identityToken);
+
     }
 
     @Override
     public void onDestroy() {
+        if (mCursor != null) {
+            mCursor.close();
+        }
     }
 }
--- app/src/main/java/com/udacity/stockhawk/StockHawkWidgetService.java
+++ app/src/main/java/com/udacity/stockhawk/StockHawkWidgetService.java
@@ -1,12 +1,15 @@
 package com.udacity.stockhawk;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.widget.RemoteViewsService;
 
 /**
  * Created by Amr Ayoub on 5/6/2017.
  */
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class StockHawkWidgetService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
